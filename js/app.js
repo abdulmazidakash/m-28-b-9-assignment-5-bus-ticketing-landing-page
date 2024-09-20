44e358d4b6983b839d277c982b6644482330a265
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-09-21 01:03:38 +0600

    project complete

--- js/app.js
+++ js/app.js
@@ -8,6 +8,10 @@ const couponInputEl = document.getElementById('coupon-field');
 const couponBtnEl = document.getElementById('coupon-btn');
 const defaultTextEl = document.getElementById('default-text');
 const grandTotalEl = document.getElementById('grand-total');
+const phoneNumberEl = document.getElementById('phone-number');
+const nextBtnEl = document.getElementById('next-btn');
+
+
 
 let selectedSeat = [];
 let totalPrice = 0;
@@ -77,6 +81,32 @@ function handleSelectSeat(event){
 			couponSave = totalPrice * 0.20;
 		}
 
+		const showCouponPriceEl = document.getElementById('show-coupon-price');
+		showCouponPriceEl.innerHTML = `
+		<p>Discount</p>
+		<p>
+			<span>-BDT: </span>
+			<span>${couponSave.toFixed(2)}</span>
+		</p>
+		
+		`
+
 		const grandTotalValue = totalPrice - couponSave;
 		grandTotalEl.innerText = grandTotalValue.toFixed(2);
+	});
+
+	phoneNumberEl.addEventListener('input', function(e){
+		const inputValue = e.target.value;
+		// console.log(inputValue);
+
+		if (inputValue.length >= 11){
+			nextBtnEl.removeAttribute('disabled');
+
+		}
+
+
+	})
+
+	document.getElementById('btn-continue').addEventListener('click', function(){
+		window.location.reload();
 	})
